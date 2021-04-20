29bf5cdbb48de3c1dcd1f7551dc75e7204f13d7a
nerdychap <hlnsiy007@myuct.ac.za>
2021-04-20 23:52:13 +0200

    adding scroll to top when tab clicked

--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import { useScrollToTop } from '@react-navigation/native'
+import React, { useRef } from 'react'
+import { ScrollView } from 'react-native'
 import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native'
 import { useShoppingItems } from '../context/Providers/ShoppingItemsProvider'
 import Item from './Item'
 
 const Shopping = () => {
     const { shoppingItems, error, hasError, isLoading } = useShoppingItems()
+    const ref = useRef(null)
+    useScrollToTop(ref)
     if (isLoading) return <View style={styles.indicatorContainer}><ActivityIndicator size="large" color="lightblue" animating /></View>
     if (hasError) return <View style={styles.indicatorContainer}><Text style={styles.errorText}>{error.name}: {error.message}</Text></View>
     return (
-        <View >
-            <FlatList 
-            data={shoppingItems} 
-            renderItem={({item}) => <Item item={item} />}
-            keyExtractor={item => item.id.toString()}
-            />
-        </View>
+                <FlatList 
+                    ref={ref}
+                    data={shoppingItems} 
+                    renderItem={({item}) => <Item item={item} />}
+                    keyExtractor={item => item.id.toString()}
+                />
     )
 }
 
